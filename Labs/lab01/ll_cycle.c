7880b82068ecd12ab37829e511a6206ebc6eff59
root <root@LAPTOP-VF4VBQNF>
2023-11-27 23:36:08 +0800

    Lab01 Basic C completed.

--- Labs/lab01/ll_cycle.c
+++ Labs/lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    if (head == NULL) {
+    	return 0;
+	}
+    node* tortoise = head;
+    node* hare = head->next;
+    while(tortoise != hare) {
+    	if (hare == NULL || hare->next == NULL) {
+    		return 0;
+		} else {
+			tortoise = tortoise->next;
+			hare = hare->next->next;
+		}
+	}
+    return 1;
 }
