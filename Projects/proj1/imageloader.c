95c60db756662ad6d2fce8498b929d2448e7ad9d
FSReed <1714747596@qq.com>
2023-12-08 11:33:00 +0800

    Project A1 completed

--- Projects/proj1/imageloader.c
+++ Projects/proj1/imageloader.c
@@ -26,17 +26,68 @@
 Image *readData(char *filename) 
 {
 	//YOUR CODE HERE
-	fscanf
+	FILE *fp = fopen(filename, "r");
+	char buff[20];
+	uint32_t column, row;
+	uint8_t pixel;
+	fscanf(fp, "%s", buff);
+	fscanf(fp, "%u %u", &column, &row);
+	fscanf(fp, "%hhu", &pixel);
+	
+	// Now we're going to get the image data.
+	Image* image = (Image*) malloc(sizeof(Image));
+	image->rows = row;
+	image->cols = column;
+	image->image = (Color**) malloc(sizeof(Color*) * image->rows);
+	int i, j;
+	uint32_t R, G, B;
+	for (i = 0; i < image->rows; i++) {
+
+		// Forget this line at first, which causes Segmentation Fault.
+		image->image[i] = (Color*) malloc(sizeof(Color) * image->cols);
+		
+		for (j = 0; j < image->cols; j++) {
+			fscanf(fp, "%u %u %u", &R, &G, &B);
+			Color* tmp = &image->image[i][j];
+			tmp->R = (uint8_t)R;
+			tmp->G = (uint8_t)G;
+			tmp->B = (uint8_t)B;
+		}
+	}
+	// Close the file.
+	fclose(fp);
+
+	return image;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
 	//YOUR CODE HERE
+	printf("P3\n");
+	printf("%d %d\n", image->cols, image->rows);
+	printf("%d\n", 255);
+	int i, j;
+	for (i = 0; i < image->rows; i++) {
+		for (j = 0; j <image->cols; j++) {
+			Color* tmp = &(image->image[i][j]);
+			printf("%3d %3d %3d", tmp->R, tmp->G, tmp->B);
+			if (j != image->cols - 1) {
+				printf("   ");
+			}
+		}
+		printf("\n");
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
 	//YOUR CODE HERE
+	int i;
+	for (i = 0; i < image->rows; i++) {
+		free(image->image[i]);
+	}
+	free(image->image);
+	free(image);
 }
