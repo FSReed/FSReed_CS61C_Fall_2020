5c618a7318ffda22ece17e0ff21f556d26ef4536
FSReed <1714747596@qq.com>
2023-12-21 19:14:17 +0800

    Lab03 completed

--- Labs/lab03/list_map.s
+++ Labs/lab03/list_map.s
@@ -2,7 +2,7 @@
 
 .text
 main:
-    jal ra, create_default_list
+    jal create_default_list
     add s0, a0, x0  # a0 = s0 is head of node list
 
     #print the list
@@ -17,7 +17,7 @@ main:
 
     # load the address of the function in question into a1 (check out la on the green sheet)
     ### YOUR CODE HERE ###
-
+    la a1, square
     # issue the call to map
     jal ra, map
 
@@ -34,6 +34,11 @@ main:
 map:
     # Prologue: Make space on the stack and back-up registers
     ### YOUR CODE HERE ###
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw, s0, 4(sp)
+    sw, s1, 8(sp)
+    #End of Prologue
 
     beq a0, x0, done    # If we were given a null pointer (address 0), we're done.
 
@@ -46,30 +51,44 @@ map:
     # load the value of the current node into a0
     # THINK: why a0?
     ### YOUR CODE HERE ###
+    lw a0, 0(s0)
 
     # Call the function in question on that value. DO NOT use a label (be prepared to answer why).
     # What function? Recall the parameters of "map"
     ### YOUR CODE HERE ###
+    addi sp, sp, -4
+    sw a1, 0(sp)
+    jalr a1
 
     # store the returned value back into the node
     # Where can you assume the returned value is?
     ### YOUR CODE HERE ###
+    sw a0, 0(s0)
 
     # Load the address of the next node into a0
     # The Address of the next node is an attribute of the current node.
     # Think about how structs are organized in memory.
     ### YOUR CODE HERE ###
+    lw s0, 4(s0)
+    mv a0, s0
 
     # Put the address of the function back into a1 to prepare for the recursion
     # THINK: why a1? What about a0?
     ### YOUR CODE HERE ###
-
+    lw a1, 0(sp)
+    addi sp, sp, 4
+    
     # recurse
     ### YOUR CODE HERE ###
+    jal map
 
 done:
     # Epilogue: Restore register values and free space from the stack
     ### YOUR CODE HERE ###
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
 
     jr ra # Return to caller
 
