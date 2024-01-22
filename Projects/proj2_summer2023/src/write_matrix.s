8ce198270e5b0bfa4ea9027a2a5ae6a8db777547
FSReed <1714747596@qq.com>
2024-01-22 10:38:15 +0800

    Proj2 write_matrix completed

--- Projects/proj2_summer2023/src/write_matrix.s
+++ Projects/proj2_summer2023/src/write_matrix.s
@@ -25,16 +25,80 @@
 write_matrix:
 
     # Prologue
+    addi sp, sp, -24
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
 
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
+    mv s3, a3
 
+    # Open the file
+    mv a0, s0
+    li a1, 1            # Write-only code is 1
+    jal ra, fopen
+    li t0, -1
+    beq a0, t0, fopen_failed
+    mv s0, a0           # Now s0 stores the file descriptor
 
+    # Write the number of rows and cols into the file
+    li a0, 8
+    jal ra, malloc
+    beq a0, zero, malloc_failed
+    mv s4, a0           # Now s4 is used to store the numbers into the memory
+    sw s2, 0(s4)
+    sw s3, 4(s4)
 
+    mv a0, s0
+    mv a1, s4
+    li a2, 2
+    li a3, 4
+    jal ra, fwrite
+    li t0, 2
+    bne a0, t0, fwrite_failed
 
+    # Write the matrix into the file
+    mul s2, s2, s3
+    mv a0, s0
+    mv a1, s1
+    mv a2, s2
+    li a3, 4
+    jal ra, fwrite
+    bne a0, s2, fwrite_failed
 
-
-
+    # Close the file
+    mv a0, s0
+    jal ra, fclose
+    bnez a0, fclose_failed
 
     # Epilogue
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    addi sp, sp, 24
 
     jr ra
+
+malloc_failed:
+    li a0, 26
+    j exit
+
+fopen_failed:
+    li a0, 27
+    j exit
+
+fclose_failed:
+    li a0, 28
+    j exit
+
+fwrite_failed:
+    li a0, 30
+    j exit
