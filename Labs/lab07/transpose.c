494412861d36f95d98ed08cfde1081d763c661f9
FSReed <1714747596@qq.com>
2024-03-09 22:05:18 +0800

    Cache blocking in transposition completed

--- Labs/lab07/transpose.c
+++ Labs/lab07/transpose.c
@@ -13,4 +13,31 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+	int row = 0, col = 0;
+	for (row = 0; row < n; row += blocksize) {
+		for (col = 0; col < n; col += blocksize) {
+			for (int y = col; y < col + blocksize; y++) {
+				for (int x = row; x < row + blocksize; x++) {
+					dst[y + x * n] = src[x + y * n];
+				}
+			}
+		}
+	}
+	row -= blocksize;
+	col -= blocksize;
+	if (row < n - 1) {
+		// Fill in the other elements
+		int x, y;
+		for (y = col; y < n; y++) {
+			for (x = 0; x < row; x++) {
+				dst[y + x * n] = src[x + y * n];
+			}
+		}
+		for (x = row; x < n; x++) {
+			for (y = 0; y < n; y++) {
+				dst[y + x * n] = src[x + y * n];
+			}
+		}
+	}
 }
+
