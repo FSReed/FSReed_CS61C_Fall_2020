d5713d664da3ab33f4a3fa02777d9d6fc988ad85
FSReed <1714747596@qq.com>
2024-05-04 13:29:38 +0800

    Format all the codes

--- Projects/proj4/matrix.c
+++ Projects/proj4/matrix.c
@@ -26,9 +26,9 @@
  * __m256d _mm256_max_pd (__m256d a, __m256d b)
  */
 
-/*
- * Generates a random double between `low` and `high`.
- */
+ /*
+  * Generates a random double between `low` and `high`.
+  */
 double rand_double(double low, double high) {
     double range = (high - low);
     double div = RAND_MAX / range;
@@ -38,12 +38,12 @@ double rand_double(double low, double high) {
 /*
  * Generates a random matrix with `seed`.
  */
-void rand_matrix(matrix *result, unsigned int seed, double low, double high) {
+void rand_matrix(matrix* result, unsigned int seed, double low, double high) {
     srand(seed);
     for (int i = 0; i < result->rows; i++) {
-	for (int j = 0; j < result->cols; j++) {
-	    set(result, i, j, rand_double(low, high));
-	}
+        for (int j = 0; j < result->cols; j++) {
+            set(result, i, j, rand_double(low, high));
+        }
     }
 }
 
@@ -57,7 +57,7 @@ void rand_matrix(matrix *result, unsigned int seed, double low, double high) {
  * failure, then remember to set it in numc.c.
  * Return 0 upon success and non-zero upon failure.
  */
-int allocate_matrix(matrix **mat, int rows, int cols) {
+int allocate_matrix(matrix** mat, int rows, int cols) {
     /* TODO: YOUR CODE HERE */
     return allocate_matrix_ref(mat, NULL, 0, 0, rows, cols);
 }
@@ -69,46 +69,46 @@ int allocate_matrix(matrix **mat, int rows, int cols) {
  * If you don't set python error messages here upon failure, then remember to set it in numc.c.
  * Return 0 upon success and non-zero upon failure.
  */
-int allocate_matrix_ref(matrix **mat, matrix *from, int row_offset, int col_offset,
-	int rows, int cols) {
+int allocate_matrix_ref(matrix** mat, matrix* from, int row_offset, int col_offset,
+    int rows, int cols) {
     /* TODO: YOUR CODE HERE */
     if (rows <= 0 || cols <= 0) {
-	PyErr_SetString(PyExc_IndexError, "Rows and cols should be positive numbers"); 
-	PyErr_Print();
-	return -1;
+        PyErr_SetString(PyExc_IndexError, "Rows and cols should be positive numbers");
+        PyErr_Print();
+        return -1;
     }
 
     if (from != NULL && ((rows + row_offset) > from->rows || (cols + col_offset) > from->cols)) {
-	PyErr_SetString(PyExc_IndexError, "Index out of bounds");
-	PyErr_Print();
-	return -1;
+        PyErr_SetString(PyExc_IndexError, "Index out of bounds");
+        PyErr_Print();
+        return -1;
     }
 
-    *mat = (matrix*) malloc(sizeof(matrix));
-    double** data = (double**) malloc(sizeof(double*) * rows);
+    *mat = (matrix*)malloc(sizeof(matrix));
+    double** data = (double**)malloc(sizeof(double*) * rows);
 
     if (from == NULL) {
-	for (int r = 0; r < rows; r++) {
-	    double* rData = (double*) malloc(sizeof(double) * cols);
-	    if (rData == NULL) {
-		PyErr_SetString(PyExc_RuntimeError, "Fail to allocate the data of the matrix");
-		PyErr_Print();
-		return -1;
-	    }
-	    for (int c = 0; c < cols; c++) {
-		rData[c] = 0.0;
-	    }
-	    data[r] = rData;
-	}
+        for (int r = 0; r < rows; r++) {
+            double* rData = (double*)malloc(sizeof(double) * cols);
+            if (rData == NULL) {
+                PyErr_SetString(PyExc_RuntimeError, "Fail to allocate the data of the matrix");
+                PyErr_Print();
+                return -1;
+            }
+            for (int c = 0; c < cols; c++) {
+                rData[c] = 0.0;
+            }
+            data[r] = rData;
+        }
     } else {
-	for (int r = 0; r < rows; r++) {
-	    double* rData = from->data[r + row_offset] + col_offset;
-	    data[r] = rData;
-	}
+        for (int r = 0; r < rows; r++) {
+            double* rData = from->data[r + row_offset] + col_offset;
+            data[r] = rData;
+        }
     }
 
     if (from != NULL) {
-	from->ref_cnt += 1;
+        from->ref_cnt += 1;
     }
 
     (*mat)->rows = rows;
@@ -128,21 +128,21 @@ int allocate_matrix_ref(matrix **mat, matrix *from, int row_offset, int col_offs
  * referring this data array.
  * See the spec for more information.
  */
-void deallocate_matrix(matrix *mat) {
+void deallocate_matrix(matrix* mat) {
     /* TODO: YOUR CODE HERE */
     if (mat == NULL || mat->ref_cnt > 1) {
-	return;
+        return;
     }
     if (mat->parent != NULL) {
-	free(mat->data);
-	mat->parent->ref_cnt -= 1;
-	free(mat);
+        free(mat->data);
+        mat->parent->ref_cnt -= 1;
+        free(mat);
     } else {
-	for (int r = 0; r < mat->rows; r++) {
-	    free(mat->data[r]);
-	}
-	free(mat->data);
-	free(mat);
+        for (int r = 0; r < mat->rows; r++) {
+            free(mat->data[r]);
+        }
+        free(mat->data);
+        free(mat);
     }
 }
 
@@ -150,7 +150,7 @@ void deallocate_matrix(matrix *mat) {
  * Return the double value of the matrix at the given row and column.
  * You may assume `row` and `col` are valid.
  */
-double get(matrix *mat, int row, int col) {
+double get(matrix* mat, int row, int col) {
     /* TODO: YOUR CODE HERE */
     return mat->data[row][col];
 }
@@ -159,7 +159,7 @@ double get(matrix *mat, int row, int col) {
  * Set the value at the given row and column to val. You may assume `row` and
  * `col` are valid
  */
-void set(matrix *mat, int row, int col, double val) {
+void set(matrix* mat, int row, int col, double val) {
     /* TODO: YOUR CODE HERE */
     mat->data[row][col] = val;
 }
@@ -167,12 +167,12 @@ void set(matrix *mat, int row, int col, double val) {
 /*
  * Set all entries in mat to val
  */
-void fill_matrix(matrix *mat, double val) {
+void fill_matrix(matrix* mat, double val) {
     /* TODO: YOUR CODE HERE */
     for (int r = 0; r < mat->rows; r++) {
-	for (int c = 0; c < mat->cols; c++) {
-	    mat->data[r][c] = val;
-	}
+        for (int c = 0; c < mat->cols; c++) {
+            mat->data[r][c] = val;
+        }
     }
 }
 
@@ -180,12 +180,12 @@ void fill_matrix(matrix *mat, double val) {
  * Store the result of adding mat1 and mat2 to `result`.
  * Return 0 upon success and a nonzero value upon failure.
  */
-int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
+int add_matrix(matrix* result, matrix* mat1, matrix* mat2) {
     /* TODO: YOUR CODE HERE */
     if (mat1->rows != mat2->rows || mat1->cols != mat2->cols) {
-	PyErr_SetString(PyExc_ValueError, "Different dimension of two given matrices");
-	PyErr_Print();
-	return -1;
+        PyErr_SetString(PyExc_ValueError, "Different dimension of two given matrices");
+        PyErr_Print();
+        return -1;
     }
     // allocate_matrix(&result, mat1->rows, mat1->cols);
     __m256d tmp_A = _mm256_setzero_pd();
@@ -193,18 +193,18 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     __m256d current = _mm256_setzero_pd();
 
     for (int r = 0; r < mat1->rows; r++) {
-	for (int c = 0; c < mat1->cols / 4; c++) {
-	    // double tmp1 = get(mat1, r, c);
-	    // double tmp2 = get(mat2, r, c);
-	    // set(result, r, c, tmp1 + tmp2);
-	    tmp_A = _mm256_loadu_pd(mat1->data[r] + c * 4);
-	    tmp_B = _mm256_loadu_pd(mat2->data[r] + c * 4);
-	    current = _mm256_add_pd(tmp_A, tmp_B);
-	    _mm256_storeu_pd(result->data[r] + c * 4, current);
-	}
-	for (int c = mat1->cols / 4 * 4; c < mat1->cols; c++) {
-	    set(result, r, c, get(mat1, r, c) + get(mat2, r, c));
-	}
+        for (int c = 0; c < mat1->cols / 4; c++) {
+            // double tmp1 = get(mat1, r, c);
+            // double tmp2 = get(mat2, r, c);
+            // set(result, r, c, tmp1 + tmp2);
+            tmp_A = _mm256_loadu_pd(mat1->data[r] + c * 4);
+            tmp_B = _mm256_loadu_pd(mat2->data[r] + c * 4);
+            current = _mm256_add_pd(tmp_A, tmp_B);
+            _mm256_storeu_pd(result->data[r] + c * 4, current);
+        }
+        for (int c = mat1->cols / 4 * 4; c < mat1->cols; c++) {
+            set(result, r, c, get(mat1, r, c) + get(mat2, r, c));
+        }
     }
     return 0;
 }
@@ -213,7 +213,7 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
  * Store the result of subtracting mat2 from mat1 to `result`.
  * Return 0 upon success and a nonzero value upon failure.
  */
-int sub_matrix(matrix *result, matrix *mat1, matrix *mat2) {
+int sub_matrix(matrix* result, matrix* mat1, matrix* mat2) {
     /* TODO: YOUR CODE HERE */
     matrix* tmp;
     allocate_matrix(&tmp, mat2->rows, mat2->cols);
@@ -228,7 +228,7 @@ int sub_matrix(matrix *result, matrix *mat1, matrix *mat2) {
  * Return 0 upon success and a nonzero value upon failure.
  * Remember that matrix multiplication is not the same as multiplying individual elements.
  */
-int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
+int mul_matrix(matrix* result, matrix* mat1, matrix* mat2) {
     /* TODO: YOUR CODE HERE */
 
     /* By Me: */
@@ -239,9 +239,9 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
      * To use the cache locality better, the innermost loop should be iterating over k when using tranposed mat2!
      */
     if (mat1->cols != mat2->rows) {
-	PyErr_SetString(PyExc_ValueError, "Improper dimensions of two matrices to do multiplication");
-	PyErr_Print();
-	return -1;
+        PyErr_SetString(PyExc_ValueError, "Improper dimensions of two matrices to do multiplication");
+        PyErr_Print();
+        return -1;
     }
     // Wrong!:
     // allocate_matrix(&result, mat1->rows, mat2->cols);
@@ -253,28 +253,28 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     transpose_matrix(transpose, mat2);
 
     __m256d current_result, tmp_result, mat1_elm, mat2_elm;
-    double* tmp_array = (double*) malloc(sizeof(double) * 4);
+    double* tmp_array = (double*)malloc(sizeof(double) * 4);
 
     for (int i = 0; i < result->rows; i++) {
-	for (int j = 0; j < result->cols; j++) {
+        for (int j = 0; j < result->cols; j++) {
 
-	    current_result = _mm256_setzero_pd();
+            current_result = _mm256_setzero_pd();
 
-	    for (int k = 0; k < mat1->cols / 4; k++) {
-		// result->data[i][j] += mat1->data[i][k] * mat2->data[k][j];
-		mat1_elm = _mm256_loadu_pd(mat1->data[i] + k * 4);
-		mat2_elm = _mm256_loadu_pd(transpose->data[j] + k * 4);
-		tmp_result = _mm256_mul_pd(mat1_elm, mat2_elm);
-		current_result = _mm256_add_pd(tmp_result, current_result);
-	    }
-	    _mm256_storeu_pd(tmp_array, current_result);
-	    for (int p = 0; p < 4; p++) {
-		result->data[i][j] += tmp_array[p];
-	    }
-	    for (int k = mat1->cols / 4 * 4; k < mat1->cols; k++) {
-		result->data[i][j] += mat1->data[i][k] * transpose->data[j][k];
-	    }
-	}
+            for (int k = 0; k < mat1->cols / 4; k++) {
+                // result->data[i][j] += mat1->data[i][k] * mat2->data[k][j];
+                mat1_elm = _mm256_loadu_pd(mat1->data[i] + k * 4);
+                mat2_elm = _mm256_loadu_pd(transpose->data[j] + k * 4);
+                tmp_result = _mm256_mul_pd(mat1_elm, mat2_elm);
+                current_result = _mm256_add_pd(tmp_result, current_result);
+            }
+            _mm256_storeu_pd(tmp_array, current_result);
+            for (int p = 0; p < 4; p++) {
+                result->data[i][j] += tmp_array[p];
+            }
+            for (int k = mat1->cols / 4 * 4; k < mat1->cols; k++) {
+                result->data[i][j] += mat1->data[i][k] * transpose->data[j][k];
+            }
+        }
     }
 
     deallocate_matrix(transpose);
@@ -287,12 +287,12 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
  * Return 0 upon success and a nonzero value upon failure.
  * Remember that pow is defined with matrix multiplication, not element-wise multiplication.
  */
-int pow_matrix(matrix *result, matrix *mat, int pow) {
+int pow_matrix(matrix* result, matrix* mat, int pow) {
     /* TODO: YOUR CODE HERE */
     if (mat->rows != mat->cols) {
-	PyErr_SetString(PyExc_ValueError, "Given Matrix is not a square matrix");
-	PyErr_Print();
-	return -1;
+        PyErr_SetString(PyExc_ValueError, "Given Matrix is not a square matrix");
+        PyErr_Print();
+        return -1;
     }
     int mat_size = mat->rows;
     matrix* tmp;
@@ -301,9 +301,9 @@ int pow_matrix(matrix *result, matrix *mat, int pow) {
     copy_data(tmp, mat);
 
     while (pow > 1) {
-	pow--;
-	mul_matrix(result, tmp, mat);
-	copy_data(tmp, result);
+        pow--;
+        mul_matrix(result, tmp, mat);
+        copy_data(tmp, result);
     }
     return 0;
 }
@@ -312,19 +312,19 @@ int pow_matrix(matrix *result, matrix *mat, int pow) {
  * Store the result of element-wise negating mat's entries to `result`.
  * Return 0 upon success and a nonzero value upon failure.
  */
-int neg_matrix(matrix *result, matrix *mat) {
+int neg_matrix(matrix* result, matrix* mat) {
     /* TODO: YOUR CODE HERE */
     // allocate_matrix(&result, mat ->rows, mat ->cols);
     __m256d tmp = _mm256_setzero_pd();
 
     for (int r = 0; r < mat->rows; r++) {
-	for (int c = 0; c < mat->cols / 4; c++) {
-	    tmp = _mm256_loadu_pd(mat->data[r] + c * 4);
-	    _mm256_storeu_pd(result->data[r] + c * 4, tmp);
-	}
-	for (int c = mat->cols / 4 * 4; c < mat->cols; c++) {
-	    result->data[r][c] = -mat->data[r][c];
-	}
+        for (int c = 0; c < mat->cols / 4; c++) {
+            tmp = _mm256_loadu_pd(mat->data[r] + c * 4);
+            _mm256_storeu_pd(result->data[r] + c * 4, tmp);
+        }
+        for (int c = mat->cols / 4 * 4; c < mat->cols; c++) {
+            result->data[r][c] = -mat->data[r][c];
+        }
     }
     return 0;
 }
@@ -333,23 +333,23 @@ int neg_matrix(matrix *result, matrix *mat) {
  * Store the result of taking the absolute value element-wise to `result`.
  * Return 0 upon success and a nonzero value upon failure.
  */
-int abs_matrix(matrix *result, matrix *mat) {
+int abs_matrix(matrix* result, matrix* mat) {
     /* TODO: YOUR CODE HERE */
     // allocate_matrix(&result, mat ->rows, mat ->cols);
     for (int r = 0; r < mat->rows; r++) {
-	for (int c = 0; c < mat->cols; c++) {
-	    int tmp = mat->data[r][c];
-	    result->data[r][c] = tmp >= 0 ? tmp : -tmp;
-	}
+        for (int c = 0; c < mat->cols; c++) {
+            int tmp = mat->data[r][c];
+            result->data[r][c] = tmp >= 0 ? tmp : -tmp;
+        }
     }
     return 0;
 }
 
 void copy_data(matrix* dest, matrix* src) {
     for (int r = 0; r < src->rows; r++) {
-	for (int c = 0; c < src->cols; c++) {
-	    dest->data[r][c] = src->data[r][c];
-	}
+        for (int c = 0; c < src->cols; c++) {
+            dest->data[r][c] = src->data[r][c];
+        }
     }
 }
 
@@ -357,20 +357,20 @@ int transpose_matrix(matrix* result, matrix* mat) {
 
     /* This piece of code comes from lab07 */
     if (result->rows != mat->cols || result->cols != mat->rows) {
-	PyErr_SetString(PyExc_RuntimeError, "Can't transpose!");
-	PyErr_Print();
-	return -1;
+        PyErr_SetString(PyExc_RuntimeError, "Can't transpose!");
+        PyErr_Print();
+        return -1;
     }
     int row = 0, col = 0, blocksize = 20;
     for (row = 0; row < mat->rows; row += blocksize) {
-	for (col = 0; col < mat->cols; col += blocksize) {
-	    for (int x = row; x < row + blocksize && x < mat->rows; x++) {
-		for (int y = col; y < col + blocksize && y < mat->cols; y++) {
-		    /* dst[x + y * n] = src[x * n + y]; */
-		    result->data[y][x] = mat->data[x][y];
-		}
-	    }
-	}
+        for (col = 0; col < mat->cols; col += blocksize) {
+            for (int x = row; x < row + blocksize && x < mat->rows; x++) {
+                for (int y = col; y < col + blocksize && y < mat->cols; y++) {
+                    /* dst[x + y * n] = src[x * n + y]; */
+                    result->data[y][x] = mat->data[x][y];
+                }
+            }
+        }
     }
     return 0;
 }
--- Projects/proj4/numc.h
+++ Projects/proj4/numc.h
@@ -7,35 +7,35 @@
  * is of type PyObject
  */
 typedef struct {
-    PyObject_HEAD
-    matrix* mat;
-    PyObject *shape;
+	PyObject_HEAD
+		matrix* mat;
+	PyObject* shape;
 } Matrix61c;
 
 /* Function definitions */
-int init_rand(PyObject *self, int rows, int cols, unsigned int seed, double low, double high);
-int init_fill(PyObject *self, int rows, int cols, double val);
-int init_1d(PyObject *self, int rows, int cols, PyObject *lst);
-int init_2d(PyObject *self, PyObject *lst);
-void Matrix61c_dealloc(Matrix61c *self);
-PyObject *Matrix61c_new(PyTypeObject *type, PyObject *args, PyObject *kwds);
-int Matrix61c_init(PyObject *self, PyObject *args, PyObject *kwds);
-PyObject *Matrix61c_to_list(Matrix61c *self);
-PyObject *Matrix61c_repr(PyObject *self);
-PyObject *Matrix61c_set_value(Matrix61c *self, PyObject* args);
-PyObject *Matrix61c_get_value(Matrix61c *self, PyObject* args);
-PyObject *Matrix61c_add(Matrix61c* self, PyObject* args);
-PyObject *Matrix61c_sub(Matrix61c* self, PyObject* args);
-PyObject *Matrix61c_multiply(Matrix61c* self, PyObject *args);
-PyObject *Matrix61c_neg(Matrix61c* self);
-PyObject *Matrix61c_abs(Matrix61c *self);
-PyObject *Matrix61c_pow(Matrix61c *self, PyObject *pow, PyObject *optional);
+int init_rand(PyObject* self, int rows, int cols, unsigned int seed, double low, double high);
+int init_fill(PyObject* self, int rows, int cols, double val);
+int init_1d(PyObject* self, int rows, int cols, PyObject* lst);
+int init_2d(PyObject* self, PyObject* lst);
+void Matrix61c_dealloc(Matrix61c* self);
+PyObject* Matrix61c_new(PyTypeObject* type, PyObject* args, PyObject* kwds);
+int Matrix61c_init(PyObject* self, PyObject* args, PyObject* kwds);
+PyObject* Matrix61c_to_list(Matrix61c* self);
+PyObject* Matrix61c_repr(PyObject* self);
+PyObject* Matrix61c_set_value(Matrix61c* self, PyObject* args);
+PyObject* Matrix61c_get_value(Matrix61c* self, PyObject* args);
+PyObject* Matrix61c_add(Matrix61c* self, PyObject* args);
+PyObject* Matrix61c_sub(Matrix61c* self, PyObject* args);
+PyObject* Matrix61c_multiply(Matrix61c* self, PyObject* args);
+PyObject* Matrix61c_neg(Matrix61c* self);
+PyObject* Matrix61c_abs(Matrix61c* self);
+PyObject* Matrix61c_pow(Matrix61c* self, PyObject* pow, PyObject* optional);
 
 /* Custom func */
 int parse_subscript(Matrix61c* self, PyObject* key,
-			Py_ssize_t* row_offset,
-			Py_ssize_t* col_offset,
-			Py_ssize_t* row,
-			Py_ssize_t* col);
+	Py_ssize_t* row_offset,
+	Py_ssize_t* col_offset,
+	Py_ssize_t* row,
+	Py_ssize_t* col);
 void parse_basic_info(PyObject* key, Py_ssize_t* start, Py_ssize_t* length);
 
