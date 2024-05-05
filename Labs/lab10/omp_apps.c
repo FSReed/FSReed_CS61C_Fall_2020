95277cd43952cad1902f82ddcac0fb4cca08b0c2
FSReed <1714747596@qq.com>
2024-05-05 19:06:46 +0800

    Lab10 Part1 Exercise3 completed!

--- Labs/lab10/omp_apps.c
+++ Labs/lab10/omp_apps.c
@@ -105,7 +105,7 @@ double dotp_reduction_optimized(double* x, double* y, int arr_size) {
   double global_sum = 0.0;
 #pragma omp parallel
   {
-#pragma omp for
+#pragma omp for reduction(+ : global_sum)
     for (int i = 0; i < arr_size; i++)
 #pragma omp critical
       global_sum += x[i] * y[i];
